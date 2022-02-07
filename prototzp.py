69f96682b26002fdddf83c999c6e34d73fed034f
Gallofok <62996896+Gallofok@users.noreply.github.com>
2022-02-07 10:22:32 +0100

    Update prototzp.py
    update the process to handel 3mf file

--- prototzp.py
+++ prototzp.py
@@ -2,12 +2,12 @@ import scipy
 from matplotlib import cm
 import pandas as pd
 import numpy as np
-
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+import shutil
+import xml.etree.cElementTree as et
+import zipfile
 from stl import mesh
-
-
 
 # # Load the STL files and add the vectors to the plot
 
@@ -39,53 +39,117 @@ from stl import mesh
 # plt.show()
 
 
-#the second method using the average popints of the meshing triangle. The idea is to set the height value of the required coordinate point to the nearest meshvalue(height)
-your_mesh = mesh.Mesh.from_file('beispiellinse.stl')
-print(your_mesh.points.shape)
-#using  the center of meshing triangle
-centerofmeshtriangle_x = (your_mesh.points[:,0]+your_mesh.points[:,3]+your_mesh.points[:,6])/3
-centerofmeshtriangle_y = (your_mesh.points[:,1]+your_mesh.points[:,4]+your_mesh.points[:,7])/3
-centerofmeshtriangle_z = (your_mesh.points[:,2]+your_mesh.points[:,5]+your_mesh.points[:,8])/3
-print(centerofmeshtriangle_x.shape)
+# the second method using the average popints of the meshing triangle.
+
+#this is the process tp read stl file
+# your_mesh = mesh.Mesh.from_file('beispiellinse.stl')
+# dmin = 200
+# print(your_mesh.points.shape)
+# centerofmeshtriangle_x = (your_mesh.points[:, 0] + your_mesh.points[:, 3] + your_mesh.points[:, 6]) / 3
+# centerofmeshtriangle_y = (your_mesh.points[:, 1] + your_mesh.points[:, 4] + your_mesh.points[:, 7]) / 3
+# centerofmeshtriangle_z = (your_mesh.points[:, 2] + your_mesh.points[:, 5] + your_mesh.points[:, 8]) / 3
+# print(centerofmeshtriangle_x.shape)
+#
+# x_range = np.concatenate([your_mesh.points[:, 0], your_mesh.points[:, 3], your_mesh.points[:, 6]])
+# y_range = np.concatenate([your_mesh.points[:, 1], your_mesh.points[:, 4], your_mesh.points[:, 7]])
+# z_range = np.concatenate([your_mesh.points[:, 2], your_mesh.points[:, 5], your_mesh.points[:, 8]])
 
 
-x_range = np.concatenate([your_mesh.points[:,0],your_mesh.points[:,3],your_mesh.points[:,6]])
+
+
+#this is process to extract the geometry information from 3mf file
+shutil.copy('beispiellinse.3mf','jg.zip')
+with zipfile.ZipFile("jg.zip","r") as zip_ref:
+    zip_ref.extractall()
+# archive = zipfile.ZipFile('jg.zip', 'r')
+# modelfile = archive.read('3D/3dmodel.model')
+
+tree =et.parse('3D/3dmodel.model')
+root=tree.getroot()
+vertictes = []
+xgather = []
+ygather = []
+zgather = []
+for x in root[0][1][0][0]:
+    vertictes.append(x.attrib)
+    for key, value in x.items():
+        if key == 'x':
+            xgather.append(float(value))
+        if key == 'y':
+            ygather.append(float(value))
+        if key == 'z':
+            zgather.append(float(value))
+
+
+x_range = np.array(xgather)
+y_range = np.array(ygather)
+z_range = np.array(zgather)
+
+
+
+
+
 xmin = np.min(x_range)
 xmax = np.max(x_range)
 
-y_range = np.concatenate([your_mesh.points[:,1],your_mesh.points[:,4],your_mesh.points[:,7]])
 ymin = np.min(y_range)
 ymax = np.max(y_range)
 
-print(xmin,xmax)
-print(ymin,ymax)
-#in this position you can set the dx,and dy
+
+zmin = np.min(z_range)
+zmax = np.max(z_range)
+
+print(xmin, xmax)
+print(ymin, ymax)
+print(zmin, zmax)
+print(type(x_range))
 dx = 5
 dy = 5
-#this method find the inded of  meshtriangle ,which meet the requirements ,x<=0.3,y<=0.2
-def finddindex(a, b, meshcenter):
 
+
+def finddindex(a, b, meshcenter):
     for xid in range(len(meshcenter[0])):
-        if(np.abs((meshcenter[0,xid] - a))<=0.3 and np.abs((meshcenter[1,xid] - b))<=0.2):
+        if np.abs((meshcenter[0, xid] - a)) <= 0.3 and np.abs((meshcenter[1, xid] - b)) <= 0.3:
             return xid
     return
 
-xline = np.arange(xmin,xmax,dx)
-yline = np.arange(ymin,ymax,dy)
 
-meshc = np.stack((centerofmeshtriangle_x,centerofmeshtriangle_y,centerofmeshtriangle_z))
-# Z array stores the height information of given(x,y)  
-Z = np.ones((len(xline),len(yline)))
-xx,yy = np.meshgrid(xline,yline)
+xline = np.arange(xmin, xmax, dx)
+yline = np.arange(ymin, ymax, dy)
+
+# meshc = np.stack((centerofmeshtriangle_x,centerofmeshtriangle_y,centerofmeshtriangle_z))
+meshc = np.stack((x_range, y_range, z_range))
+
+Z = np.ones((len(xline), len(yline)))
+xx, yy = np.meshgrid(xline, yline)
 for xi in range(len(xline)):
     for yi in range(len(yline)):
-        if (finddindex(xline[xi],yline[yi],meshc)!=None):
-            Z[xi,yi] = meshc[2,finddindex(xline[xi],yline[yi],meshc)]
-
+        if finddindex(xline[xi], yline[yi], meshc) is not None:
+            Z[xi, yi] = meshc[2, finddindex(xline[xi], yline[yi], meshc)]
 
 print(Z)
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-ax.scatter3D(xx, yy, Z, c=Z, cmap='Greens');
+ax.scatter3D(xx, yy, Z, c=Z, cmap='Greens')
 plt.show()
+
+##the belowed code for showing the plot of original stl file
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+
+# Create a new plot
+figure = pyplot.figure()
+axes = mplot3d.Axes3D(figure)
+
+# Load the STL files and add the vectors to the plot
+your_mesh = mesh.Mesh.from_file('beispiellinse.stl')
+axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+
+# Auto scale to the mesh size
+scale = your_mesh.points.flatten()
+axes.auto_scale_xyz(scale, scale, scale)
+
+# Show the plot to the screen
+pyplot.show()
