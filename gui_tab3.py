b75d79626bbfd425acea1fb07cd26137648594b5
Gallofok <62996896+Gallofok@users.noreply.github.com>
2022-05-03 11:14:19 +0200

    Update gui_tab3.py

--- gui_tab3.py
+++ gui_tab3.py
@@ -1,12 +1,18 @@
+from calendar import c
+import threading
 from tkinter import *
 import tkinter.filedialog
-
+import sys
 from matplotlib.pyplot import connect
 from sklearn.preprocessing import scale
 import round_controller
 import reccontrol
 import serial
 import time
+import numpy as np
+import chr_dll2_connection as chr_connection
+import re
+
 
 class tab3:
 
@@ -17,8 +23,8 @@ class tab3:
         self.barbeginn = 0
         self.barend = 500
         self.buttonframe = LabelFrame(self.frame)
-        self.portframe = LabelFrame(self.frame)
-        self.measureframe = LabelFrame(self.frame)
+        self.portframe = LabelFrame(self.frame,width=400,height=200)
+        self.measureframe = LabelFrame(self.frame,width=400,height=200)
         self.myLabel = Label(self.frame, text=' ')
         self.e = Entry(self.buttonframe, width=20, font=('Helvetica', 25))
 
@@ -27,10 +33,10 @@ class tab3:
         self.scrollbar = Scrollbar(self.portframe)
         self.scrollbar.pack( side = RIGHT, fill = Y )
 
-        self.mylist = Listbox(self.portframe, yscrollcommand = self.scrollbar.set )
+        self.mylist = Listbox(self.portframe, yscrollcommand = self.scrollbar.set,width=67)
         for line in range(6):
             self.mylist.insert(END, "COM" + str(line))
-        self.mylist.pack()
+        self.mylist.pack(fill=X)
         self.scrollbar.config( command = self.mylist.yview )
 
 
@@ -52,10 +58,12 @@ class tab3:
         self.ycor.grid(row = 1,column=1)
         self.L2 = Label(self.measureframe, text="y")
         self.L2.grid(row = 1,column=0)
-        self.measure = Button(self.measureframe,text='measurebeginn')
+        self.measure = Button(self.measureframe,text='measurebeginn',command=lambda:threading.Thread(target=self.measureprocess).start())
+        self.emstop = Button(self.measureframe,text='pause',command = self.stoppro,width=26)
+
         self.measure.grid(row = 2,column=1)
-        self.measure.bind('<Button-1>',self.measureprocess)
-        self.measureframe.grid(row = 3,column=3)
+        self.emstop.grid(row = 3,column=1)
+        self.measureframe.grid(row = 3,column=1)
 
 
         # add some buttons
@@ -97,7 +105,7 @@ class tab3:
 
         # put the button in the plattform
 
-        self.buttonframe.grid(row=0,column=0)
+        #self.buttonframe.grid(row=0,column=0)
         self.Buttonx.grid(row=0, column=0)
         self.Buttony.grid(row=0, column=1)
         self.Buttonz.grid(row=0, column=2)
@@ -124,7 +132,7 @@ class tab3:
         self.jj.grid(row=1, column=0)
         self.jg = LabelFrame(self.frame)
         self.jg.grid(row=1, column=1)
-
+        
 
         self.control = round_controller.round_controller(self.jj, self.jj)
         """
@@ -157,20 +165,96 @@ class tab3:
         self.control2.canvas.tag_bind(self.control2.trilist[3], '<Button-1>', self.zmovingplus0)
         self.control2.canvas.tag_bind(self.control2.trilist[4], '<Button-1>', self.zmovingplus1)
         self.control2.canvas.tag_bind(self.control2.trilist[5], '<Button-1>', self.zmovingplus2)
-    def measureprocess(self,cmd):
+
+
+        #this code is for the detector
+        self.chr = chr_connection.CHR_connection('IP: 169.254.2.217',1)
+        self.res = self.chr.send_command('$MMD 0')
+        self.n_sample = 100
+
+        """
+        Label
+        """
+        self.labe = LabelFrame(self.frame)
+        self.display_text = StringVar()
+        self.display = Label(self.labe, textvariable=self.display_text,width=60, height=10)
+        self.display.grid(row=0, columnspan=3)
+        self.labe.grid(row=4,column=0)
+
+        
+    def add_txt(self,cmd):
+        s = self.display_text.get()
+        fid = [go.start() for go in re.finditer('\n',s)]
+        if(len(fid)==5):
+            s = ' '
+        s += cmd+'\n'
+        self.display_text.set(s)
+
+
+    def gettheintensiti(self):
+            self.intensity = self.chr.send_command('$SODX 257')
+            self.chr.set_autobuffer_size(self.n_sample)
+            self.chr.flush_autobuffer()    
+            self.chr.start_autobuffer()
+            self.buffer = self.chr.read_autobuffer()
+            self.intenmean = np.mean(self.buffer)
+            return self.intenmean
+    def getthedistance(self):
+            self.distan = self.chr.send_command('$SODX 256')
+            self.chr.set_autobuffer_size(self.n_sample)
+            self.chr.flush_autobuffer()    
+            self.chr.start_autobuffer()
+            self.buffer2 = self.chr.read_autobuffer()
+            self.dismean = np.mean(self.buffer2)
+            return self.dismean   
+
+    def measureprocess(self):
         try:
-            self.ser.write(str.encode("G90\r\n"))
-            self.ser.write(str.encode("G01"+'X'+self.xcor.get()+'Y'+self.ycor.get()))
+            #self.ser.reset_input_buffer()
             self.ser.write(str.encode("G91\r\n"))
-            for i in range(10):
-                self.ser.write(str.encode("G01"+'X'+'1'))
+            if self.xcor.get() == '':
+                self.xcor.get() == 0
+            if self.ycor.get() == '':
+                self.ycor.get() == 0
+            self.ser.write(str.encode("G01"+'X'+self.xcor.get()+'Y'+self.ycor.get()+'\r\n'))
+            self.ser.write(str.encode("M0 P2500\r\n"))
+
+            for i in range(25):
+                print(self.ser.readline().decode("utf-8"))
+                #self.ser.write(str.encode("M0 P3500\r\n"))
+                print('moving to the position')
+                self.ser.write(str.encode("G01"+'X'+'1'+'\r\n'))
+                cod = 0
+                while (cod<25000) :
+                    #self.ser.write(str.encode("G01"+'Z'+'-0.1''\r\n'))
+                    self.ser.write(str.encode("M0 P1000\r\n"))
+                    cod = self.gettheintensiti()
+                    #print('dis now is ' + str(cod) + ' um')
+                    print(cod)
+                    time.sleep(0.1)
+                
+                print('distance is'+ ' : '+ str(self.getthedistance())+ ' ' + 'um')
+                #self.ser.write(str.encode("G01"+'Z'+'5'+'\r\n'))
+                time.sleep(3.5)
+
+        except serial.serialutil.PortNotOpenError:
+            print('port closed')            
+            self.add_txt('port closed')   
         except AttributeError:
             print('no machine connected')
+            self.add_txt('no machine connected')
+    
 
-    def hel(self, k):
-        print('this func is used to test if the widget between two tag can communcate')
-
-
+    def stoppro(self):
+        if(self.ser != None):
+            print("disconnect")
+            self.add_txt('disconnect')
+            self.ser.close()
+            self.ser = None
+        else:
+            print('no port connected now')
+            self.add_txt('no port connected now')
+    
     def linearvelcontrol(self, cmd):
         self.barbegin = 60
         self.barend = 500
@@ -194,8 +278,13 @@ class tab3:
         try:
             self.ser = serial.Serial(self.mylist.selection_get(), 115200)
             print(self.mylist.selection_get()+' '+"connected")
+            self.add_txt(self.mylist.selection_get()+' '+"connected")
         except serial.serialutil.SerialException:
             print('port'+ ' '+ self.mylist.selection_get() +' '+'can not connect' )
+            self.add_txt('port'+ ' '+ self.mylist.selection_get() +' '+'can not connect' )
+        except tkinter.TclError:
+            print('one port muss be selected first')
+            self.add_txt('one port muss be selected first')
 
 
     def Homexyz(self):
@@ -203,22 +292,34 @@ class tab3:
             self.ser.write(str.encode("G28\r\n"))
         except AttributeError:
             print('no machine connected')
+            self.add_txt('no machine connected')
     def disconnect3d(self):
+        
         try:
-            print("disconnect")
-            self.ser.close()
-            self.ser = None
+            if(self.ser != None):
+                print("disconnect")
+                self.add_txt('disconnect')
+                self.ser.close()
+                self.ser = None
+            else:
+                print('no port connected now')
+                self.add_txt('no port connected now')
         except AttributeError:
             print('no machine connected')
-
+            self.add_txt('no machine connected')   
     def moving(self,direction,scale):
-        print(str(direction)+''+str(scale))
+        
         try:
+            print(str(direction)+''+str(scale))
             self.ser.write(str.encode("G91\r\n"))
             self.ser.write(str.encode("G01"+direction+scale+"\r\n"))
+            self.add_txt(str(direction)+''+str(scale))
         except AttributeError:
             print("moving machine not conneted yet")
-
+            self.add_txt("moving machine not conneted yet")
+        except serial.serialutil.SerialException:
+            print("no connection")
+            self.add_txt("no connection")
 
     def xmovingminus0(self,default = 0 ):
         dir = 'X'
@@ -273,7 +374,7 @@ class tab3:
         self.moving(direction=dir, scale=sc)
 
     def ymovingminus2(self, default=0):
-        dir = 'y'
+        dir = 'Y'
         sc = '-10'
         self.moving(direction=dir, scale=sc)
 
