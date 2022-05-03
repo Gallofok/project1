c4fce7587e408dd9960071df407c309d19e06691
Gallofok <62996896+Gallofok@users.noreply.github.com>
2022-05-03 13:47:49 +0200

    Update reccontrol.py

--- reccontrol.py
+++ reccontrol.py
@@ -5,7 +5,7 @@ class reccontrol():
 
         self.root = root
         self.frame = frame
-        self.canvas = Canvas(self.frame, width=150, height=350, borderwidth=0, highlightthickness=0)
+        self.canvas = Canvas(self.frame, width=200, height=400, borderwidth=0, highlightthickness=0)
 
         self.colorlis = ["#2ECC71", "#F4D03F", "#5DADE2"]  # green yellow blue
         self.colorlishighlight = ["#28B463", "#F1C40F", "#3498DB"]  # corresponding colors
@@ -13,18 +13,17 @@ class reccontrol():
         for i in range(6):
             
             if np.abs(i-2.5) < 1:
-                self.trilist.append(self.canvas.create_rectangle(0,i*50,100,50+i*50,fill = self.colorlis[0],activefill = self.colorlishighlight[0]))
+                self.trilist.append(self.canvas.create_rectangle(50,50+i*50,50+100,50+50+i*50,fill = self.colorlis[0],activefill = self.colorlishighlight[0]))
                 
             
             if np.abs(i-2.5) < 2 and np.abs(i-2.5) > 1:
-                 self.trilist.append(self.canvas.create_rectangle(0,i*50,100,50+i*50,fill = self.colorlis[1],activefill = self.colorlishighlight[1]))
+                 self.trilist.append(self.canvas.create_rectangle(50+0,50+i*50,50+100,50+50+i*50,fill = self.colorlis[1],activefill = self.colorlishighlight[1]))
             
             if np.abs(i-2.5) > 2:
-                 self.trilist.append(self.canvas.create_rectangle(0,i*50,100,50+i*50,fill = self.colorlis[2],activefill = self.colorlishighlight[2]))
+                 self.trilist.append(self.canvas.create_rectangle(50+0,50+i*50,50+100,50+50+i*50,fill = self.colorlis[2],activefill = self.colorlishighlight[2]))
 
-        self.canvas.create_text(125,50,text='-z',font='Helvetica 15 bold')
-        self.canvas.create_text(125,250,text='+z',font='Helvetica 15 bold')
-        self.gg = self.trilist
+        self.canvas.create_text(175,100,text='-z')
+        self.canvas.create_text(175,300,text='+z')
         self.canvas.pack()
 
 
